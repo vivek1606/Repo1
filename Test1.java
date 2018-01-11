28ad7208c675fb3ff8a80810b6633721b3dfb905
vivek1606 <vivek.1606@gmail.com>
2018-01-11 23:53:15 +0530

    added Files

---
+++ Test1.java
@@ -0,0 +1,6 @@
+class Test1 {
+   public static void main(String[] args) {
+      // Prints "Test1" in the terminal window.
+      System.out.println("TestOne");
+   }
+}
---
+++ Test3.java
@@ -0,0 +1,6 @@
+class Test3 {
+   public static void main(String[] args) {
+      // Prints "Test3" in the terminal window.
+      System.out.println("Test3");
+   }
+}
